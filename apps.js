80390ce8bb723a8536448b85ea8e3363533985bb
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-04-01 12:45:17 -0700

    Commit 1 - Created tomagochi class and started styling

---
+++ apps.js
@@ -0,0 +1,25 @@
+console.log("JS linked")
+// Instatiate your Tomagotchi
+// Display a character of your choice on the screen to represent your pet
+// Add the ability to name your pet.
+    // maybe use prompt?
+// Increase your pet's age every x minutes
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// Your pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Morph your pet at certain ages.
+// Animate your pet across the screen while it's alive.
+
+
+
+
+
+
+
+
+
+
+// ### Extras
+// * Have your tomagotchi give birth to baby tomagotchi...
+// * ...with special powers (extend the class)!
+// * Add an `excercise()` method to your tomagotchi, that affects certain properties
+// * Add anything you can think of... use your imagination!
---
+++ toma.js
@@ -0,0 +1,27 @@
+// Create a Class (JS Class, look at your notes if your forget) for your tomagotchi
+// Increase your pet's age every x minutes
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// Your pet should die if Hunger, Boredom, or Sleepiness hits 10.
+class TomagotchiPet {
+    constructor(name){
+        this.name = name;
+        this.hunger = ;
+        this.boredom = ;
+        this.sleepiness = ;
+    }
+    gethungry(){
+
+    }
+    getBored(){
+
+    }
+    getSleepy(){
+        
+    }
+    age(){
+
+    }
+    die(){
+
+    }
+}
